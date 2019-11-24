4bd47e96a1f8514b29d922eb85f9636f5b78a649
Liviu Schera <liviu115@gmail.com>
2019-11-24 20:24:45 +0930

    Updates

---
+++ precache-manifest.feb39b3c18d092b67b499e261acd0742.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5fdf0f3a151b2db82486163dc18f1ab",
+    "url": "/react-api-practice/index.html"
+  },
+  {
+    "revision": "4e310a2f80039800c429",
+    "url": "/react-api-practice/static/css/main.5192c706.chunk.css"
+  },
+  {
+    "revision": "32dae7019ce77fe63282",
+    "url": "/react-api-practice/static/js/2.6f3d3ad2.chunk.js"
+  },
+  {
+    "revision": "4e310a2f80039800c429",
+    "url": "/react-api-practice/static/js/main.cd297fc4.chunk.js"
+  },
+  {
+    "revision": "1d48c1592d7898063c9f",
+    "url": "/react-api-practice/static/js/runtime-main.7605b440.js"
+  }
+]);
